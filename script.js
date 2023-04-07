ccbcb37ccf74a3e76c37f04ba959b476ae450989
yole <igbavagodswill4@gmail.com>
2023-04-07 22:04:57 +0100

    made the mobile nav bar and also included functionalities

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 'use strict';
 
+//navbar toggle variable and functions
+const openNav = document.querySelector('.openNav');
+const closeNav = document.querySelector('.closeNav');
+const navBar = document.querySelector('.navBar');
+
+openNav.addEventListener('click', function() {
+    closeNav.classList.remove('hide');
+    openNav.classList.add('hide');
+    navBar.style.display = 'flex';
+});
+
+closeNav.addEventListener('click', function() {
+    closeNav.classList.add('hide');
+    openNav.classList.remove('hide');
+    navBar.style.display = 'none';
+});
+
+// default style
+
+// window.onload = function() {
+//     navBar.style.display = 'none';
+// };
