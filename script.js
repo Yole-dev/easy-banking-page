ea19010d6daa0f90df472e26f03848b1dd4a93e3
yole <igbavagodswill4@gmail.com>
2023-04-09 15:51:56 +0100

    done creating desktop view

--- script.js
+++ script.js
@@ -18,6 +18,65 @@ closeNav.addEventListener('click', function() {
     navBar.style.display = 'none';
 });
 
+// for desktop navbar hover effects
+const home = document.querySelector('.home');
+const homeDrop = document.querySelector('.homeDrop');
+
+const about = document.querySelector('.about');
+const aboutDrop = document.querySelector('.aboutDrop');
+
+const contact = document.querySelector('.contact');
+const contactDrop = document.querySelector('.contactDrop');
+
+const blog = document.querySelector('.blog');
+const blogDrop = document.querySelector('.blogDrop');
+
+const careers = document.querySelector('.careers');
+const careersDrop = document.querySelector('.careersDrop');
+
+home.addEventListener('mouseover', function() {
+    homeDrop.style.opacity = '1';
+});
+
+home.addEventListener('mouseout', function() {
+    homeDrop.style.opacity = '0';
+});
+
+about.addEventListener('mouseover', function() {
+    aboutDrop.style.opacity = '1';
+});
+
+about.addEventListener('mouseout', function() {
+    aboutDrop.style.opacity = '0';
+});
+
+contact.addEventListener('mouseover', function() {
+    contactDrop.style.opacity = '1';
+});
+
+contact.addEventListener('mouseout', function() {
+    contactDrop.style.opacity = '0';
+});
+
+blog.addEventListener('mouseover', function() {
+    blogDrop.style.opacity = '1';
+});
+
+blog.addEventListener('mouseout', function() {
+    blogDrop.style.opacity = '0';
+});
+
+careers.addEventListener('mouseover', function() {
+    careersDrop.style.opacity = '1';
+});
+
+careers.addEventListener('mouseout', function() {
+    careersDrop.style.opacity = '0';
+});
+
+
+
+
 // default style
 
 // window.onload = function() {
